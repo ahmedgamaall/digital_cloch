0848e2e1e684c64e374cb00f19efbfa39c893b76
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2023-10-04 23:16:19 +0300

    complete app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/my_theme_provider.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => MyThemeModel(),
+        child: Consumer<MyThemeModel>(
+          builder: (context, theme, child) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Analog Clock',
+            theme: themeData(context),
+            darkTheme: darkThemeData(context),
+            themeMode: theme.isLightTheme ? ThemeMode.light : ThemeMode.dark,
+            home: const HomeScreen(),
+          ),
+        ));
+  }
+}
